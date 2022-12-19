a1bc4c420ede153403cbbad17d452c3432dfb5f8
Janmark Sabanal <80690430+janmarksabanal@users.noreply.github.com>
2022-12-20 02:31:21 +0800

    Update data.dart

--- lib/data.dart
+++ lib/data.dart
@@ -6,7 +6,7 @@ class Data {
       urlAvatar:
           'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
       username: 'George',
-      message: 'Father.',
+      message: 'Father',
     ),
     Chat(
       urlAvatar:
@@ -16,7 +16,7 @@ class Data {
     ),
     Chat(
       urlAvatar:
-          'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+          'https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1153&q=80',
       username: 'Ricky',
       message: 'Brother',
     ),
@@ -37,6 +37,12 @@ class Data {
           'https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1153&q=80',
       username: 'Junrick',
       message: 'Brother',
+    ),
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1153&q=80',
+      username: 'Janmark',
+      message: 'Myself',
     ),
   ];
 }

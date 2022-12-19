0c675a279a9e434488a148d66e8674ab33a21060
Janmark Sabanal <jsabanal80@gmail.com>
2022-12-20 02:10:01 +0800

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,42 @@
+import 'model/chat.dart';
+
+class Data {
+  static const chats = <Chat>[
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+      username: 'George',
+      message: 'Father.',
+    ),
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1569913486515-b74bf7751574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80',
+      username: 'Aida',
+      message: 'Mother',
+    ),
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      username: 'Ricky',
+      message: 'Brother',
+    ),
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1588979355313-6711a095465f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=621&q=80',
+      username: 'Catherine',
+      message: 'Sister',
+    ),
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1153&q=80',
+      username: 'Kevin',
+      message: 'Brother',
+    ),
+    Chat(
+      urlAvatar:
+          'https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1153&q=80',
+      username: 'Junrick',
+      message: 'Brother',
+    ),
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:midterm/data.dart';
+import 'package:midterm/model/chat.dart';
+import 'package:midterm/utils.dart';
+import 'package:midterm/widget/dimissible_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String title = 'Sabanal Hands-On Exam';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<Chat> items = List.of(Data.chats);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.restore),
+              onPressed: () {
+                items = List.of(Data.chats);
+              },
+            ),
+          ],
+        ),
+        body: ListView.separated(
+          itemCount: items.length,
+          separatorBuilder: (context, index) => Divider(),
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return DismissibleWidget(
+                item: item,
+                child: buildListTile(item),
+                onDismissed: (direction) =>
+                    dismissItem(context, index, direction));
+          },
+        ),
+      );
+
+  void dismissItem(
+    BuildContext context,
+    int index,
+    DismissDirection direction,
+  ) {
+    setState(() {
+      items.removeAt(index);
+    });
+
+    switch (direction) {
+      case DismissDirection.endToStart:
+        Utils.showSnackBar(context, 'Chat has been deleted');
+        break;
+      case DismissDirection.startToEnd:
+        Utils.showSnackBar(context, 'Chat has been archived');
+        break;
+      default:
+        break;
+    }
+  }
+
+  Widget buildListTile(Chat item) => ListTile(
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 16,
+        ),
+        leading: CircleAvatar(
+          radius: 28,
+          backgroundImage: NetworkImage(item.urlAvatar),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              item.username,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 4),
+            Text(item.message)
+          ],
+        ),
+        onTap: () {},
+      );
+}
